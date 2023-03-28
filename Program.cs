e449edbcac85b90fd18f64ea4d1a1643f43cce36
Alex Vlasov <drumlife182@gmail.com>
2023-03-28 13:43:48 +0200

    Added other digital numbers

--- Program.cs
+++ Program.cs
@@ -71,6 +71,53 @@ class DigitalNumbers
           new [] {" ", " ", "|"},
           new [] {" ", " ", "|"},
         },
+        '2' => new string[][]
+       {
+          new [] {" ", "_", " "},
+          new [] {" ", "_", "|"},
+          new [] {"|", "", "_"}},
+        '3' => new string[][]
+        {
+          new [] {" ", "_", " "},
+          new [] {" ", "_", "|"},
+          new [] {" ", " _", "|"}
+        },
+        '4' => new string[][]
+        {
+          new [] {" ", " ", " "},
+          new [] {"|", "_", "|"},
+          new [] {" ", " ", "|"},
+        },
+        '5' => new string[][]
+        {
+          new [] {" ", " ", "_"},
+          new [] {" |", "_", " "},
+          new [] {" ", " _", "|"}
+        },
+        '6' => new string[][]
+        {
+          new [] {" ", " ", " _"},
+          new [] {" |", "_", " "},
+          new [] {" |", "_", "|"}
+        },
+        '7' => new string[][]
+        {
+          new [] {" ", " _", " "},
+          new [] {" ", " ", "|"},
+          new [] {" ", " ", "|"}
+        },
+        '8' => new string[][]
+        {
+          new [] {" ", " _", " "},
+          new [] {" |", "_", "|"},
+          new [] {" |", "_", "|"}
+        },
+        '9' => new string[][]
+        {
+          new [] {" ", " _", " "},
+          new [] {" |", "_", "|"},
+          new [] {" ", " _", "|"}
+        },
         _ => throw new ArgumentOutOfRangeException(nameof(c), c, "no matrix exsists for taht int")
       };
     }
